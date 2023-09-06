028e07932c55f34f31fa64f8fd5f1d502ac712e8
TamirKol <tamirk9596@gmail.com>
2023-09-06 21:16:39 +0300

    starting filter

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -4,6 +4,17 @@ import { MailsData } from './mailsData.js'
 import { storageService } from '../../../services/storage.service.js'
 const MAIL_KEY = 'mailDB'
 
+const loggedinUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+const criteria = {
+    status: 'inbox/sent/trash/draft',
+    txt: '', 
+    isRead: true, // (optional property, if missing: show all)
+    isStared: true, // (optional property, if missing: show all)
+    lables: ['important', 'romantic'] // has any of the labels
+   }
 _createMails()
 export const mailService = {
     query,
@@ -11,11 +22,15 @@ export const mailService = {
     remove,
     save,
     getEmptyMail,
-    
+    getDefaultFilter,
+
 }
 
-function query(){
-    return  asyncStorageService.query(MAIL_KEY)
+function query(filterBy = criteria) {
+    return asyncStorageService.query(MAIL_KEY).then(mails=>{
+        if(filterBy){}
+    })
+
 }
 function get(mailId) {
     return asyncStorageService.get(MAIL_KEY, mailId)
@@ -33,17 +48,33 @@ function save(mail) {
     }
 }
 
+function getDefaultFilter(){
+    return{
+        status:'',
+        txt: '',
+        isRead:null,
+        isStared:null,
+        lables:[]
+    }
+}
+// function setFilterBy(filterBy = {}) {
+//     if (filterBy.txt !== undefined) gFilterBy.title = filterBy.title
+//     if (filterBy.price !== undefined) gFilterBy.price = filterBy.price
+//     if (filterBy.publishedDate !== undefined)
+//       gFilterBy.publishedDate = filterBy.publishedDate
+//     return gFilterBy
+//   }
+
 function getNextMailId(mailId) {
     return asyncStorageService.query(MAIL_KEY).then(mails => {
-      var idx = mails.findIndex(book => book.id === bookId)
-      if (idx === books.length - 1) idx = -1
-      return books[idx + 1].id
+        var idx = mails.findIndex(book => book.id === bookId)
+        if (idx === books.length - 1) idx = -1
+        return books[idx + 1].id
     })
-  }
+}
 
 function getEmptyMail() {
     return {
-        id: '',
         subject: '',
         body: '',
         isRead: false,
@@ -56,10 +87,9 @@ function getEmptyMail() {
 
 function _createMails() {
     let mails = storageService.loadFromStorage(MAIL_KEY)
-    console.log('my demo mails',mails)
     if (!mails || !mails.length) {
-     mails= MailsData.getMails()
-     storageService.saveToStorage(MAIL_KEY,mails)
+        mails = MailsData.getMails()
+        storageService.saveToStorage(MAIL_KEY, mails)
     }
 }
 
--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -2,12 +2,15 @@ import { MailList } from "../cmps/MailList.jsx"
 import { mailService } from "../services/mail.service.js"
 import {showErrorMsg, showSuccessMsg} from '../../../services/event-bus.service.js'
 import { EmailCompose } from "../cmps/EmailCompose.jsx"
+import { MailFilter } from "../cmps/EmailFilter.jsx"
 
 const { useState, useEffect } = React
 
 export function MailIndex() {
     const [mails, setMails] = useState(null)
     const [isAddingMail, setIsAddingMail] = useState(false)
+    const [filterBy, setFilterBy] = useState(mailService.getDefaultFilter())
+
 
 
     useEffect(() => {
@@ -15,6 +18,11 @@ export function MailIndex() {
             .then(mails => setMails(mails))
             .catch((err) => console.log(err))
     }, [])
+    useEffect(() => {
+        mailService.query()
+            .then(mails => setMails(mails))
+            .catch((err) => console.log(err))
+    }, [filterBy])
 
     function onRemoveMail(mailId) {
         mailService.remove(mailId)
@@ -28,14 +36,18 @@ export function MailIndex() {
             })
     }
 
-   function onComposeMail(mail){
-    console.log(mail)
-   }
+    function onSetFilterBy(filterBy) {
+        
+        setFilterBy(prevFilter => ({ ...prevFilter, ...filterBy }))
+        console.log(filterBy)
+    }   
+   
     if (!mails) return <div>loading...</div>
     return (
         <section>
+            <MailFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
             <button onClick={()=>setIsAddingMail(!isAddingMail)}>add mail</button>
-            {isAddingMail&& <EmailCompose onComposeMail={onComposeMail}/>}
+            {isAddingMail&& <EmailCompose />}
             <MailList mails={mails} onRemoveMail={onRemoveMail} />
         </section>
 
