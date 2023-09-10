369a96b8c01c8df577ea668dcd89cf4ad8d07e0d
Tal Elias <talelias2@gmail.com>
2023-09-10 03:52:50 +0300

    design final touch

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -85,7 +85,7 @@ function _createNotes() {
                     backgroundColor: '#98FB98'
                 },
                 info: {
-                    url: 'assets/img/dan-gold.jpg',
+                    url: 'https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80',
                     title: 'Bobi and Me'
                 }
             },
